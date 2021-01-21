983b4383b96933c24af835e636ccc6c6c2a01fad
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-21 19:34:07 +0100

    Add IQuery - Actions whic query the system

---
+++ Application/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    // Actions that requesting the representation of data from our system
+    public interface IQuery<TSearch, TResult> : IUseCase
+    {
+        TResult Execute(TSearch search);
+    }
+}
