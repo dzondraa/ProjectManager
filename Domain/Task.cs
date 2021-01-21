6b31ef1044677626a25a3f5eb8e51ce9f9109c2a
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-21 20:27:44 +0100

    Add Domain layer

---
+++ Domain/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Dictionary<string, object>> Properties { get; set; } = null;
+
+    }
+}
