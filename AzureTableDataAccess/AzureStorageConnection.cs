fc0e901335760ce1d5771cf446e1fced52908e5c
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-23 14:37:55 +0100

    Implement CreateProjectCommand with TableCli class

--- AzureTableDataAccess/AzureStorageConnection.cs
+++ AzureTableDataAccess/AzureStorageConnection.cs
@@ -11,13 +11,13 @@ namespace AzureTableDataAccess
         private static CloudStorageAccount _instance;
 
 
-        public static CloudStorageAccount Instance(string connectionString, string accountName)
+        public static CloudStorageAccount Instance()
         {
             // Uses lazy initialization.
 
             if (_instance == null)
             {
-                _instance = CloudStorageAccount.Parse(connectionString);
+                _instance = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=eduaccount;AccountKey=1aum0Jx/fz/xENwYqz+j7JRTnYS5cIsUUdfZ1XvQ2R7NnoIaObJ7bg4KxInTt1IlvISRKOebtBSrroUEl43AZA==;EndpointSuffix=core.windows.net");
             }
 
             return _instance;
---
+++ Implementation/Commands/TableCliCreateProject.cs
@@ -0,0 +1,28 @@
+﻿using Application.Commands;
+using Application.DataTransfer;
+using AzureTableDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementation.Commands
+{
+    public class TableCliCreateProject : ICreateProjectCommand
+    {
+        private readonly TableCli _tableCli;
+        
+        public int Id => 1;
+
+        public string Name => "Insert or merge operation using TableCli";
+
+        public TableCliCreateProject()
+        {
+            _tableCli = new TableCli(AzureStorageConnection.Instance(), "Projects");
+        }
+
+        public void Execute(ProjectDto request)
+        {
+           
+        }
+    }
+}
