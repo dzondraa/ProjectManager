6022982e9416641b8d807c5592c10c06ed9a1ddd
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-23 14:11:26 +0100

    Add parent abstract entity with soft delete option

---
+++ AzureTableDataAccess/Entity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureTableDataAccess
+{
+    public abstract class Entity : TableEntity
+    {
+     
+        // Soft delete option
+        public DateTime? DeletedAt { get; set; }
+
+        public Entity(string partitionKey, string rowKey)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+        }
+    }
+
+ 
+}
