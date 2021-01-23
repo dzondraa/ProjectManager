a5f689f1024cb5c3012de0beeec784b90e767c65
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-23 14:15:07 +0100

    Add Project entity

---
+++ AzureTableDataAccess/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureTableDataAccess.Entities
+{
+    public class Project : Entity
+    {
+        public string Name { get; set; }
+
+        public Project(string partitionKey, string rowKey, string name) : base (partitionKey, rowKey)
+        {
+            Name = name;
+        }
+    }
+}
