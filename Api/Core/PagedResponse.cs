dbd63b383e200db298090a93958cca43ffd7f050
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-01-24 22:07:19 +0100

    Add logic for formating API response in one place

---
+++ Api/Core/PagedResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Core
+{
+    public class PagedResponse<T>
+    {
+        public int TotalCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
