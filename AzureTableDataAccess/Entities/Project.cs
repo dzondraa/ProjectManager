a0420f2e3c9ab0cd3c92d9c13dbd09ef64e5ac2d
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-02-03 16:18:39 +0100

    Edit project entity

--- AzureTableDataAccess/Entities/Project.cs
+++ AzureTableDataAccess/Entities/Project.cs
@@ -8,7 +8,6 @@ namespace AzureTableDataAccess.Entities
     public class Project : Entity
     {
         public string Name { get; set; }
-        public IEnumerable<Entities.Task> tasks { get; set; }
 
         public Project(string partitionKey, string rowKey, string name) : base (partitionKey, rowKey)
         {
