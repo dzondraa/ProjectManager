89cc177c9e4ef4335433181e9ab03f1443361bf6
Djordje Nikolic <v-dnikolic@microsoft.com>
2021-02-17 13:15:39 +0100

    Project exists pre-validation for Code file upload

---
+++ Implementation/Validatiors/FileUploadValidator.cs
@@ -0,0 +1,27 @@
+﻿using Application.Queries;
+using Application.Requests;
+using AzureTableDataAccess;
+using FluentValidation;
+using Implementation.Queries;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Implementation.Validatiors
+{
+    public class FileUploadValidator : AbstractValidator<FileRequest>
+    {
+        private readonly IQueryProject _queryProject;
+
+        public FileUploadValidator(IQueryProject queryProject)
+        {
+            _queryProject = queryProject;
+            RuleFor(x => x.ProjectName)
+                .Must(p => _queryProject.Execute(new Api.Searches.ProjectSearch { Name = p }).TotalCount > 0)
+                .WithMessage($"Could not reference the project with that name");
+              
+        }
+
+    }
+}
